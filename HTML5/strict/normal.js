2a78a56043e52ab5ecae6c7a65a90eba78d3fd12
陈嘉恺 <jiakai.chen@fu-fang.com>
2016-10-18 14:37:10 +0800

    20161018

---
+++ HTML5/strict/normal.js
@@ -0,0 +1,42 @@
+(function(parent){
+	var doc = window.document;
+	var output = doc.getElementById("normal-output");
+
+	function writeLine(string) {
+		var line = doc.createElement('div');
+		line.textContent = string;
+		output.appendChild(line);
+	}
+
+	writeLine("if (array):");
+	try {
+		writeLine(array && "true" || "false");
+	} catch (error) {
+		writeLine("error: " + error.message);
+	}
+
+	writeLine("if ('array' in window):");
+	try {
+		writeLine(('array' in window) && "true" || "false");
+	} catch (error) {
+		writeLine("error: " + error.message);
+	}
+	
+	writeLine("if (typeof(array) === 'undefined'):");
+	try {
+		writeLine((typeof(array) === 'undefined') && "true" || "false");
+	} catch (error) {
+		writeLine("error: " + error.message);
+	}
+	
+	writeLine("array = [];");
+	array = [];
+	writeLine("if (array):");
+	try {
+		writeLine(array && "true" || "false");
+	} catch (error) {
+		writeLine("error: " + error.message);
+	}
+
+	
+})(window);
